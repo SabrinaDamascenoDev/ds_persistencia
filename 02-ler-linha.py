5b35f779401dbd8018948d39d8afe80f3dc4ad94
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2025-09-15 11:23:26 -0300

    Initial commit

---
+++ 02-ler-linha.py
@@ -0,0 +1,6 @@
+with open("arquivo.txt", "r") as file:
+    linha = file.readline()
+    #Loop for print the line, without repeat code
+    while linha:
+        print(linha.rstrip())
+        linha = file.readline()
---
+++ 03-ler-arquivo.py
@@ -0,0 +1,6 @@
+with open("arquivo.txt", "r") as file:
+    #Can break the memory, but if use readline don break
+    #To work with larger files, we divide in streams to work separated
+    doc = file.read()
+
+print(doc)
